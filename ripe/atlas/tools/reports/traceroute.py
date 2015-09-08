376045f0e90e5fbf24264c70336f11d004acfe03
Daniel Quinn <code@danielquinn.org>
2015-09-08 18:11:14 +0200

    Simple support for traceroutes

--- ripe/atlas/tools/reports/traceroute.py
+++ ripe/atlas/tools/reports/traceroute.py
@@ -5,4 +5,21 @@ class TracerouteReport(Report):
 
     @staticmethod
     def format(result):
-        print("Not ready yet\n")
+
+        r = ""
+
+        for hop in result.hops:
+
+            name = ""
+            rtts = []
+            for packet in hop.packets:
+                name = name or packet.origin
+                rtts.append(packet.rtt)
+
+            r += "{:>3} {:15} {}ms\n".format(
+                hop.index,
+                name,
+                "ms  ".join(["{:6}".format(rtt) for rtt in rtts])
+            )
+
+        return r
