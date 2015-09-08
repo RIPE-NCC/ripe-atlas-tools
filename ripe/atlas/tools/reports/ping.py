5836fb5b7f7b5327b95c1de5045fe0d4155427bf
Daniel Quinn <code@danielquinn.org>
2015-09-08 13:39:04 +0200

    Cleaned up the formatter a bit

--- ripe/atlas/tools/reports/ping.py
+++ ripe/atlas/tools/reports/ping.py
@@ -13,11 +13,12 @@ class PingReport(Report):
         if not packets:
             return json.dumps(result.raw_data) + "\n"
 
-        return "{} bytes from {:15} to {} ({}): ttl={} times:{}\n".format(
+        return "{} bytes from probe #{:<5} {:15} to {} ({}): ttl={} times:{}\n".format(
             result.packet_size,
+            result.probe_id,
             packets[0].source_address,
             result.destination_name,
             result.destination_address,
             packets[0].ttl,
-            ", ".join(["{:7}".format(_.rtt) for _ in packets])
+            " ".join(["{:8}".format(str(_.rtt) + ",") for _ in packets])
         )
