4c9325aba3ed71ef2686632c4ecf745d328c9abd
Daniel Quinn <code@danielquinn.org>
2015-09-08 14:25:42 +0200

    Crude streaming support (for ping only at the moment)

---
+++ ripe/atlas/tools/commands/stream.py
@@ -0,0 +1,51 @@
+from __future__ import print_function, absolute_import
+import json
+
+from ripe.atlas.cousteau import AtlasRequest, AtlasStream
+from ripe.atlas.sagan import Result, ResultError
+
+from ..reports import Report
+from .base import Command as BaseCommand
+
+
+class Command(BaseCommand):
+
+    DESCRIPTION = "Report the results of a measurement"
+    URLS = {
+        "detail": "/api/v2/measurements/{}.json",
+        "latest": "/api/v2/measurements/{}/latest.json",
+    }
+
+    def __init__(self, *args, **kwargs):
+        BaseCommand.__init__(self, *args, **kwargs)
+        self.formatter = None
+
+    def add_arguments(self):
+        self.parser.add_argument(
+            "measurement_id",
+            type=int,
+            help="The measurement id you want streamed"
+        )
+
+    def run(self):
+
+        pk = self.arguments.measurement_id
+
+        detail = AtlasRequest(url_path=self.URLS["detail"].format(pk)).get()[1]
+
+        self.formatter = Report.get_formatter(detail["type"]["name"])
+
+        stream = AtlasStream()
+        stream.connect()
+
+        stream.bind_stream("result", self.on_result_response)
+        try:
+            stream.start_stream(stream_type="result", msm=pk)
+            stream.timeout(seconds=5)
+        except KeyboardInterrupt:
+            self.ok("Disconnecting from the stream")
+            stream.disconnect()
+
+
+    def on_result_response(self, result, *args):
+        print(self.formatter.format(Result.get(result)), end="")
