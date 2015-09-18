6a9d3e10fcb7508f1933ee5d91e3e75b9be623b4
Daniel Quinn <dquinn@ripe.net>
2015-09-18 16:43:17 +0200

    Added future notes

--- ripe/atlas/tools/reports/tls.py
+++ ripe/atlas/tools/reports/tls.py
@@ -2,6 +2,10 @@ from .base import Report
 
 
 class TlsReport(Report):
+    """
+    Somehow, we need to figure out how to make an SSL result look like the the
+    output from `openssl x509 -in w00t -noout -text`.
+    """
 
     @classmethod
     def format(cls, result, probes=None):
