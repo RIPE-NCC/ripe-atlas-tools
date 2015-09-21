bda2e8c5e82559d1d2a8d19a0600406286842292
Daniel Quinn <dquinn@ripe.net>
2015-09-21 17:53:58 +0200

    Added the 'go' command

---
+++ ripe/atlas/tools/commands/go.py
@@ -0,0 +1,23 @@
+import webbrowser
+from .base import Command as BaseCommand
+
+
+class Command(BaseCommand):
+
+    DESCRIPTION = "Visit the web page for a specific measurement"
+    URL = "https://atlas.ripe.net/measurements/{}/"
+
+    def add_arguments(self):
+        self.parser.add_argument(
+            "measurement_id",
+            type=int,
+            help="The measurement id you want reported"
+        )
+
+    def run(self):
+        url = self.URL.format(self.arguments.measurement_id)
+        if not webbrowser.open(url):
+            self.ok(
+                "It looks like your system doesn't have a web browser "
+                "available.  You'll have to go there manually: {}".format(url)
+            )
