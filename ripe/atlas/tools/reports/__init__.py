ffe63fb81422d6d42293c8706056e556b9b6e4a3
Daniel Quinn <dquinn@ripe.net>
2015-09-22 10:56:08 +0200

    s/tls/sslcert/ because reasons

--- ripe/atlas/tools/reports/__init__.py
+++ ripe/atlas/tools/reports/__init__.py
@@ -2,6 +2,6 @@ from .base import Report
 from .ping import PingReport
 from .traceroute import TracerouteReport
 from .dns import DnsReport
-from .tls import TlsReport
+from .sslcert import SslcertReport
 from .ntp import NtpReport
 from .http import HttpReport
