c7ade74053a49ed422a82f1e8667db1bb965e127
Daniel Quinn <dquinn@ripe.net>
2015-09-28 17:59:03 +0200

    Shhhhhh

---
+++ ripe/atlas/tools/commands/shibboleet.py
@@ -0,0 +1,96 @@
+from __future__ import absolute_import, print_function
+
+import random
+import requests
+
+from ..cache import cache
+from .base import Command as BaseCommand
+
+
+class Command(BaseCommand):
+
+    DESCRIPTION = "https://xkcd.com/806/"
+    HEADERS = {
+        "Accept": "application/vnd.github.v3+json",
+        "User-Agent": "RIPE Atlas Tools (Magellan)",
+    }
+    URLS = {
+        "root": "https://api.github.com",
+        "statistics": "/repos/RIPE-NCC/ripe-atlas-tools/stats/contributors",
+        "users": "/users"
+    }
+
+    def run(self):
+
+        response = requests.get(
+            "{}{}".format(self.URLS["root"], self.URLS["statistics"]),
+            headers=self.HEADERS
+        )
+
+        contributors = []
+        for contributor in response.json():
+            user = self.get_user(contributor["author"]["login"])
+            d = {
+                "changes": 0,
+                "name": user["name"] or contributor["author"]["login"],
+                "url": contributor["author"]["html_url"]
+            }
+            for week in contributor["weeks"]:
+                d["changes"] += week["a"] + week["d"] + week["c"]
+            contributors.append(d)
+
+        random.shuffle(contributors)
+
+        r = "\nThanks for using RIPE Atlas!\n\nThis toolkit " \
+            "(Magellan) is a group effort, spearheaded by the team at the " \
+            "RIPE\nNCC, but supported by members of the community from all " \
+            "over.  If you're\ncurious about who we are and what sorts of " \
+            "stuff we work on, here's a break\ndown of our contributions to " \
+            "date.\n\nName                     Changes  URL\n{}\n".format(
+            "-" * 79)
+        for contributor in contributors:
+            r += "{name:20}  {changes:10}  {url}\n".format(**contributor)
+
+        print(r + """
+                                                             |
+                                                             |
+                                                             |
+                                                     |       |
+                                                     |      ---
+                                                    ---     '-'
+                                                    '-'  ____|_____
+                                                 ____|__/    |    /
+                                                /    | /     |   /
+                                               /     |(      |  (
+                                              (      | \     |   \\
+                                               \     |  \____|____\   /|
+                                               /\____|___`---.----` .' |
+                                           .-'/      |  \    |__.--'    \\
+                                         .'/ (       |   \   |.          \\
+                                      _ /_/   \      |    \  | `.         \\
+                                       `-.'    \.--._|.---`  |   `-._______\\
+                                          ``-.-------'-------'------------/
+                                              `'._______________________.'\n""")
+
+    def get_user(self, username):
+
+        cache_key = "github-user:{}".format(username)
+
+        user = cache.get(cache_key)
+        if user:
+            return user
+
+        cache.set(
+            cache_key,
+            requests.get(
+                "{}{}/{}".format(
+                    self.URLS["root"],
+                    self.URLS["users"],
+                    username
+                ),
+                headers=self.HEADERS
+            ).json(),
+            60 * 60 * 24 * 365
+        )
+
+        return self.get_user(username)
