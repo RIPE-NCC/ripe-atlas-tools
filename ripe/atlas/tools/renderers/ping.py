48dd3e35b3eced62c5cf7cb39d2bcc733867c87e
Daniel Quinn <dquinn@ripe.net>
2015-09-30 15:51:35 +0200

    Colin's input regarding origin IPs

--- ripe/atlas/tools/renderers/ping.py
+++ ripe/atlas/tools/renderers/ping.py
@@ -13,11 +13,17 @@ class Renderer(BaseRenderer):
         if not packets:
             return "No packets found"
 
+        # Because the origin value is more reliable as "from" in v4 and as
+        # "packet.source_address" in v6.
+        origin = result.origin
+        if ":" in origin:
+            origin = packets[0].source_address
+
         line = "{} bytes from probe #{:<5} {:15} to {} ({}): ttl={} times:{}\n"
         return Result(line.format(
             result.packet_size,
             result.probe_id,
-            packets[0].source_address,
+            origin,
             result.destination_name,
             result.destination_address,
             packets[0].ttl,
