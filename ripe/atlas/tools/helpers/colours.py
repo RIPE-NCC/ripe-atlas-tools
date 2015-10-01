23ff17591f30384fda3b1841a88932cc50410d1b
Daniel Quinn <dquinn@ripe.net>
2015-10-01 15:22:19 +0200

    Moved the colour logic out into its own module

--- ripe/atlas/tools/helpers/colours.py
+++ ripe/atlas/tools/helpers/colours.py
@@ -1,1 +1,13 @@
-__author__ = 'daniel'
+class Colour(object):
+
+    light_blue = "\033[1;34m"
+    light_green = "\033[1;32m"
+    light_cyan = "\033[1;36m"
+    light_red = "\033[1;31m"
+    yellow = "\033[1;33m"
+    green = "\033[0;32m"
+    cyan = "\033[0;36m"
+    brown = "\033[0;33m"
+    pink = "\033[1;35m"
+
+    reset = "\033[0m"
