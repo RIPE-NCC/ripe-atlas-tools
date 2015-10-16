5e62981a37d24915f9f9c29ea49ec0ba637ebb90
Daniel Quinn <dquinn@ripe.net>
2015-10-16 17:46:52 +0200

    one-letter variables are a dark, dark sin

--- ripe/atlas/tools/helpers/colours.py
+++ ripe/atlas/tools/helpers/colours.py
@@ -4,44 +4,44 @@ import sys
 class Colour(object):
 
     @classmethod
-    def _colourise(cls, t, c):
-        return "{}[{}m{}{}[0m".format(chr(0x1b), c, t, chr(0x1b))
+    def _colourise(cls, text, colour):
+        return "{}[{}m{}{}[0m".format(chr(0x1b), colour, text, chr(0x1b))
 
     @classmethod
-    def black(cls, t):
-        return cls._colourise(t, 30)
+    def black(cls, text):
+        return cls._colourise(text, 30)
 
     @classmethod
-    def red(cls, t):
-        return cls._colourise(t, 31)
+    def red(cls, text):
+        return cls._colourise(text, 31)
 
     @classmethod
-    def green(cls, t):
-        return cls._colourise(t, 32)
+    def green(cls, text):
+        return cls._colourise(text, 32)
 
     @classmethod
-    def yellow(cls, t):
-        return cls._colourise(t, 33)
+    def yellow(cls, text):
+        return cls._colourise(text, 33)
 
     @classmethod
-    def blue(cls, t):
-        return cls._colourise(t, 34)
+    def blue(cls, text):
+        return cls._colourise(text, 34)
 
     @classmethod
-    def mangenta(cls, t):
-        return cls._colourise(t, 35)
+    def mangenta(cls, text):
+        return cls._colourise(text, 35)
 
     @classmethod
-    def cyan(cls, t):
-        return cls._colourise(t, 36)
+    def cyan(cls, text):
+        return cls._colourise(text, 36)
 
     @classmethod
-    def white(cls,t):
-        return cls._colourise(t, 37)
+    def white(cls, text):
+        return cls._colourise(text, 37)
 
     @classmethod
-    def bold(cls, t):
-        return cls._colourise(t, 1)
+    def bold(cls, text):
+        return cls._colourise(text, 1)
 
 
 def colourise(text, colour):
