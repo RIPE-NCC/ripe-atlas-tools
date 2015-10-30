0e1c4e52d14e2fed61c15fec479d8582db8f3081
Andreas Strikos <astrikos@ripe.net>
2015-10-30 16:17:03 +0100

    unbreak aggregate_ping renderer and add tests to it

--- ripe/atlas/tools/renderers/aggregate_ping.py
+++ ripe/atlas/tools/renderers/aggregate_ping.py
@@ -4,7 +4,7 @@ from .base import Renderer as BaseRenderer
 class Renderer(BaseRenderer):
     """
     This is meant to be a stub example for what an aggregate renderer might look
-    like.  If you have ideas as to how to make this better, feel free to send
+    like. If you have ideas as to how to make this better, feel free to send
     along a pull request.
     """
 
@@ -13,58 +13,96 @@ class Renderer(BaseRenderer):
     def __init__(self):
         self.target = ""
         self.packet_loss = 0
-        self.totals = {
-            "sent": 0,
-            "received": 0,
-            "min": [],
-            "rtts": [],
-            "max": []
+        self.sent_packets = 0
+        self.received_packets = 0
+        self.rtts = []
+        self.rtts_min = []
+        self.rtts_max = []
+        self.rtt_types_map = {
+            "min": self.rtts_min,
+            "max": self.rtts_max
         }
 
-    @staticmethod
-    def mean(rtts):
+    def header(self):
+        print("Collecting results...\n")
+
+    def additional(self, results):
+        self.collect_stats(results)
+        self.packet_loss = self.calculate_loss()
+        print(self.render(
+            "reports/aggregate_ping.txt",
+            target=self.target,
+            sent=self.sent_packets,
+            received=self.received_packets,
+            packet_loss=self.packet_loss,
+            min=min(self.rtts_min),
+            median=self.median(),
+            mean=self.mean(),
+            max=max(self.rtts_max)
+        ))
+
+    def collect_stats(self, results):
+        """
+        Calculates, stores and collects all stats we want from the given
+        results.
+        """
+        for result in results:
+            self.set_target(result)
+
+            self.sent_packets += result.packets_sent
+            self.received_packets += result.packets_received
+            self.collect_min_max_rtts("min", result.rtt_min)
+            self.collect_min_max_rtts("max", result.rtt_max)
+
+            self.collect_packets_rtt(result.packets)
+
+    def set_target(self, result):
+        """Sets the target of the measurement if not set."""
+        if not self.target:
+            self.target = result.destination_name
+
+    def collect_min_max_rtts(self, rtt_type, rtt):
+        """
+        Stores the given rtt in the corresponding list (min/max) if rtt is set.
+        """
+        rtt = rtt
+        if not rtt:
+            rtt = 0
+
+        self.rtt_types_map[rtt_type].append(rtt)
+
+    def collect_packets_rtt(self, packets):
+        """
+        Collects all the rrts of given packets and stores them
+        in our rtts list.
+        """
+        for packet in packets:
+            rtt = packet.rtt
+            if not packet.rtt:
+                rtt = 0
+            self.rtts.append(rtt)
+
+    def calculate_loss(self):
+        """Calculates the total loss between received and sent packets."""
+        if not self.sent_packets:
+            return 0
+
+        return (1 - float(self.received_packets) / self.sent_packets) * 100
+
+    def mean(self):
+        """Calculates the mean of the collected rtts"""
         return round(
-            float(sum(rtt for rtt in rtts if rtt is not None)) / max(len(rtts), 1), 3
+            float(sum(self.rtts)) / max(len(self.rtts), 1), 3
         )
 
-    @staticmethod
-    def median(rtts):
-        sorted_rtts = sorted(rtts)
-        index = (len(rtts) - 1) // 2
-        if len(rtts) % 2:
+    def median(self):
+        """Calculates the median of the collected rtts"""
+        sorted_rtts = sorted(self.rtts)
+        index = (len(self.rtts) - 1) // 2
+        if len(self.rtts) % 2:
             return sorted_rtts[index]
         else:
             return (sorted_rtts[index] + sorted_rtts[index + 1]) / 2.0
 
-    def on_start(self):
-        return "Collecting results...\n"
-
     def on_result(self, result):
-        # destination_name is included in every ping result
-        # but not exposed to on_start, on_finish via msm metadata
-        # set it only if it was empty
-        if self.target == "":
-            self.target = result.destination_name
-        self.totals["sent"] += result.packets_sent
-        self.totals["received"] += result.packets_received
-        self.totals["min"].append(result.rtt_min)
-        self.totals["max"].append(result.rtt_max)
-
-        for packet in result.packets:
-            self.totals["rtts"].append(packet.rtt)
-
         return ""
-
-    def on_finish(self):
-        self.packet_loss = (1 - float(self.totals["received"]) / self.totals["sent"]) * 100
-        return self.render(
-            "reports/aggregate_ping.txt",
-            target=self.target,
-            sent=self.totals["sent"],
-            received=self.totals["received"],
-            packet_loss=self.packet_loss,
-            min=min(min for min in self.totals["min"] if min is not None),
-            median=self.median(self.totals["rtts"]),
-            mean=self.mean(self.totals["rtts"]),
-            max=max(self.totals["max"])
-        )
