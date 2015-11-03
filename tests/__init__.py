11a6bfb059b3672f348f037fb6f456f07b124945
Daniel Quinn <code@danielquinn.org>
2015-11-03 17:21:41 +0100

    Add report, measure, and measurements to the complete list of tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,19 @@
 from .aggregators import TestAggregators
-from .commands import TestProbesCommand
+from .commands import (
+    TestProbesCommand,
+    TestMeasureCommand,
+    TestMeasurementsCommand,
+    TestReportCommand
+)
 from .helpers import TestArgumentTypeHelper
 from .renderers import TestPingRenderer
 
 
-__all__ = [TestAggregators, TestProbesCommand, TestPingRenderer]
+__all__ = [
+    TestAggregators,
+    TestProbesCommand,
+    TestMeasureCommand,
+    TestMeasurementsCommand,
+    TestReportCommand,
+    TestPingRenderer,
+]
