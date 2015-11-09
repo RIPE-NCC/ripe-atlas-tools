c70a2ccbe2e8e75d77edab5c970347d5a6b9e7b6
Emile Aben <emileaben@gmail.com>
2015-11-09 15:18:49 +0100

    new renderer for when dst is in multiple ASNs

---
+++ ripe/atlas/tools/renderers/dst_asn.py
@@ -0,0 +1,35 @@
+from .base import Renderer as BaseRenderer
+from ripe.atlas.cousteau import ProbeRequest
+from collections import Counter
+
+from ..ipdetails import IP
+
+class Renderer(BaseRenderer):
+
+    RENDERS = [BaseRenderer.TYPE_PING]
+
+    def __init__(self):
+        self.asns = Counter() # keys are timestamps, data struct captures ASN membership
+        self.asn2name = {}
+
+    def on_result(self, result):
+        dst = result.destination_address
+        if dst != None:
+           ip = IP( dst )
+           if ip.asn:
+               self.asns[ ip.asn ] += 1
+               self.asn2name[ ip.asn ] = ip.holder
+           else:
+               self.asns[ '<unknown>' ] += 1
+               self.asn2name[ '<unknown>' ] = 'unknown'
+           return ""
+        return ""
+
+    def additional(self,results):
+        total = sum(self.asns.values()) 
+        for asn,count in self.asns.most_common():
+            print "AS%s %.2f%% (%s)" % (
+              asn, 
+              100.0*count/total,
+              self.asn2name[ asn ]
+            )
