8a3a5c37e644cae3133e9a3a2f73639b4a9f9053
Emile Aben <emileaben@gmail.com>
2015-11-09 15:31:47 +0100

    pep8 ??

--- ripe/atlas/tools/renderers/dst_asn.py
+++ ripe/atlas/tools/renderers/dst_asn.py
@@ -15,21 +15,21 @@ class Renderer(BaseRenderer):
     def on_result(self, result):
         dst = result.destination_address
         if dst is not None:
-           ip = IP(dst)
-           if ip.asn:
-               self.asns[ip.asn] += 1
-               self.asn2name[ip.asn] = ip.holder
-           else:
-               self.asns['<unknown>'] += 1
-               self.asn2name['<unknown>'] = 'unknown'
+            ip = IP(dst)
+            if ip.asn:
+                self.asns[ip.asn] += 1
+                self.asn2name[ip.asn] = ip.holder
+            else:
+                self.asns['<unknown>'] += 1
+                self.asn2name['<unknown>'] = 'unknown'
            return ""
         return ""
 
-    def additional(self,results):
+    def additional(self, results):
         total = sum(self.asns.values()) 
-        for asn,count in self.asns.most_common():
+        for asn, count in self.asns.most_common():
             print "AS%s %.2f%% (%s)" % (
               asn, 
               100.0*count/total,
-              self.asn2name[ asn ]
+              self.asn2name[asn]
             )
