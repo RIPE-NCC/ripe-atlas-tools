b8e404633c40ae3f0defe00d2363183406b77139
Daniel Quinn <dquinn@ripe.net>
2015-11-19 17:48:00 +0100

    Now all types are supported

--- ripe/atlas/tools/commands/measurement.py
+++ ripe/atlas/tools/commands/measurement.py
@@ -30,7 +30,7 @@ class Command(BaseCommand):
                 "That measurement does not appear to exist")
 
         self.render_basic(measurement)
-        self.render_ping(measurement)
+        getattr(self, "render_{}".format(measurement.type.lower()))(measurement)
 
     @classmethod
     def render_basic(cls, measurement):
@@ -38,7 +38,7 @@ class Command(BaseCommand):
         cls._render(measurement, (
             ("id", "ID"),
             ("id", "URL", lambda _: colourise(url_template.format(_), "cyan")),
-            ("type", "Type", lambda _: _["name"]),
+            ("type", "Type", lambda _: cls._prettify_type(_["name"])),
             ("status", "Status", lambda _: _["name"]),
             ("description", "Description"),
             ("af", "Address Family"),
@@ -65,13 +65,102 @@ class Command(BaseCommand):
     def render_ping(cls, measurement):
         cls._render(measurement, (
             ("packets", "Packets"),
-            ("size", "Size")
+            ("size", "Size"),
         ))
+
+    @classmethod
+    def render_traceroute(cls, measurement):
+        cls._render(measurement, (
+            ("packets", "Packets"),
+            ("protocol", "Protocol"),
+            ("dont_fragment", "Don't Fragment", cls._prettify_boolean),
+            ("paris", "Paris"),
+            ("first_hop", "First Hop"),
+            ("max_hops", "Maximum Hops"),
+            ("timeout", "Timeout"),
+            ("size", "Size"),
+            ("destination_option_size", "Destination Option Size"),
+            ("hop_by_hop_option_size", "Hop-by-hop Option Size"),
+            ("gap_limit", "Gap Limit"),
+        ))
+
+    @classmethod
+    def render_dns(cls, measurement):
+        cls._render(measurement, (
+            ("query", "Query", cls._prettify_query),
+            ("retry", "Retry Times"),
+            ("include_qbuf", "Include the Qbuf?", cls._prettify_boolean),
+            ("include_abuf", "Include the Abuf?", cls._prettify_boolean),
+            ("protocol", "Protocol"),
+            ("prepend_probe_id", "Prepend the Probe ID?"),
+            ("udp_payload_size", "UDP Payload Size"),
+            (
+                "use_probe_resolver",
+                "Use the Probe's Resolver?",
+                cls._prettify_boolean
+            ),
+            ("set_do_bit", "Set the DO Bit?", cls._prettify_boolean),
+            ("set_nsid_bit", "Set the NSID Bit?", cls._prettify_boolean),
+            ("set_rd_bit", "Set the RD Bit?", cls._prettify_boolean),
+            ("set_cd_bit", "Set the CD Bit?", cls._prettify_boolean),
+        ))
+
+    @classmethod
+    def render_sslcert(cls, measurement):
+        cls._render(measurement, (
+            ("port", "Port"),
+        ))
+
+    @classmethod
+    def render_http(cls, measurement):
+        cls._render(measurement, (
+            ("header_bytes", "Header Bytes"),
+            ("version", "Version"),
+            ("method", "Method"),
+            ("port", "Port"),
+            ("path", "Path"),
+            ("query_string", "Query String"),
+            ("user_agent", "User-Agent"),
+            ("max_bytes_read", "Body Bytes"),
+        ))
+        timing_verbosity = 0
+        if "extended_timing" in measurement.meta_data:
+            if measurement.meta_data["extended_timing"]:
+                timing_verbosity = 1
+                if "more_extended_timing" in measurement.meta_data:
+                    if measurement.meta_data["more_extended_timing"]:
+                        timing_verbosity = 2
+        cls._render_line("Timing Verbosity", timing_verbosity)
+
+    @classmethod
+    def render_ntp(cls, measurement):
+        cls._render(measurement, (
+            ("packets", "Packets"),
+            ("timeout", "Timeout"),
+        ))
+
+    @staticmethod
+    def _prettify_type(kind):
+        types = {
+            "ping": "Ping",
+            "traceroute": "Traceroute",
+            "dns": "DNS",
+            "sslcert": "SSL Certificate",
+            "http": "HTTP",
+            "ntp": "NTP"
+        }
+        if kind in types:
+            return colourise(colourise(types[kind], "bold"), "blue")
+        return colourise("Unknown", "red")
 
     @staticmethod
     def _prettify_time(timestamp):
         return "{} UTC".format(
             datetime.fromtimestamp(timestamp).isoformat().replace("T", " "))
+
+    @staticmethod
+    def _prettify_query(query):
+        return "{} {} {}".format(query["class"], query["type"], query["value"])
 
     @staticmethod
     def _prettify_boolean(boolean):
@@ -87,15 +176,21 @@ class Command(BaseCommand):
         return colourise(x, "red")
 
     @staticmethod
-    def _render(measurement, keys):
+    def _render_line(header, value):
+        print("{}  {}".format(
+            colourise("{:25}".format(header), "bold"), value))
+
+    @classmethod
+    def _render(cls, measurement, keys):
         for prop in keys:
+
+            value = None
             if prop[0] in measurement.meta_data:
                 value = measurement.meta_data[prop[0]]
-                if value is None:
-                    value = "N/A"
-                elif len(prop) == 3:
-                    value = prop[2](value)
-                print("{}  {}".format(
-                    colourise("{:20}".format(prop[1]), "bold"),
-                    value
-                ))
+
+            if value is None:
+                value = "-"
+            elif len(prop) == 3:
+                value = prop[2](value)
+
+            cls._render_line(prop[1], value)
