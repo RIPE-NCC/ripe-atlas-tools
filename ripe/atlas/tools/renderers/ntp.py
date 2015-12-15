58b1293d2ba7798ceee2280439ad6d54937c51cc
Daniel Quinn <code@danielquinn.org>
2015-12-15 14:15:50 +0000

    Fixing the NTP placeholder so it at least doesn't explode

--- ripe/atlas/tools/renderers/ntp.py
+++ ripe/atlas/tools/renderers/ntp.py
@@ -14,6 +14,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from .base import Renderer as BaseRenderer
+from .base import Result
 
 
 class Renderer(BaseRenderer):
@@ -21,4 +22,4 @@ class Renderer(BaseRenderer):
     RENDERS = [BaseRenderer.TYPE_NTP]
 
     def on_result(self, result, probes=None):
-        print("Not ready yet\n")
+        return Result("Not ready yet\n", result.probe_id)
