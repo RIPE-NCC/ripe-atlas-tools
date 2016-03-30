44be617e34a4832f0c7ca9e335d5b24e081c26d4
Andreas Strikos <astrikos@ripe.net>
2016-03-30 11:35:08 +0200

    Using measure command to test bash compeltion instead of report
    The problem was that travis were failing tests (on local environment
    tests were passing) that were using report as
    an example because apparently it doesn't allow to create cache file from
    a subprocess. Most likely security reasons. Instead using measure
    command that doesn't call cache on import seems to do the trick.

--- tests/test_bash_completion.py
+++ tests/test_bash_completion.py
@@ -16,19 +16,16 @@ class BashCompletionTests(unittest.TestCase):
     def _setup_env(self, substring):
         input_str = "ripe-atlas" + substring
         os.environ['COMP_WORDS'] = input_str
-        idx = len(input_str.split(' ')) - 1  # Index of the last word
-        comp_cword = idx + 1 if substring.strip() and substring.endswith(' ') else idx
+        comp_cword = len(input_str.split(' ')) - 1  # Index of the last word
         os.environ['COMP_CWORD'] = str(comp_cword)
 
     def _autocomplete(self, substring):
 
         self._setup_env(substring)
-        cmd_parts = "ripe-atlas"
-        if substring:
-            cmd_parts = ["ripe-atlas", substring]
+        cmd_parts = "ripe-atlas" + substring
         envs = os.environ.copy()
         process = subprocess.Popen(
-            cmd_parts, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=envs
+            cmd_parts, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=envs, shell=True
         )
         output, error = process.communicate()
         return output.decode("utf-8"), error.decode("utf-8")
@@ -36,35 +33,38 @@ class BashCompletionTests(unittest.TestCase):
     def test_commands_completion(self):
         """Tests autocompletion of commands."""
         input_str = " "
-        output, _ = self._autocomplete(input_str)
-        self.assertTrue("measure" in output)
+        output, error = self._autocomplete(input_str)
+        print(output, error)
+        self.assertTrue("report" in output)
 
     def test_completion_disable(self):
         """
         Tests if autocompletion is disabled if environmental variable
         is not set.
         """
-        input_str = " "
+        input_str = " mea"
         del os.environ['RIPE_ATLAS_AUTO_COMPLETE']
-        _, error = self._autocomplete(input_str)
+        output, error = self._autocomplete(input_str)
+        print(output, error)
         self.assertTrue("No such command" in error)
 
     def test_command_completion(self):
         """Tests autocompletion of specific command."""
         input_str = " meas"
-        output, _ = self._autocomplete(input_str)
+        output, error = self._autocomplete(input_str)
+        print(output, error)
         self.assertTrue("measure" in output)
 
     def test_options_completion(self):
         """Tests autocompletion of existing options for a command."""
-        input_str = " report "
-        output, _ = self._autocomplete(input_str)
-        output = output.strip().split('\n')
-        for item in output:
-            self.assertTrue(item.startswith('--'))
+        input_str = " measure "
+        output, error = self._autocomplete(input_str)
+        print(output, error)
+        self.assertEqual(output, "dns http ntp ping sslcert traceroute")
 
     def test_option_completion(self):
         """Tests autocompletion of specific option of a command."""
-        input_str = " report --h"
-        output, _ = self._autocomplete(input_str)
+        input_str = " measure ping --h"
+        output, error = self._autocomplete(input_str)
+        print(output, error)
         self.assertEqual(output, "--help")
