294997c82f17a7fa97af5434914abd2b7a424be4
Teemu Rytilahti <tpr@iki.fi>
2017-04-10 22:01:04 +0200

    make pep8&flake happy + cleanup the code a bit

--- ripe/atlas/tools/renderers/ntp.py
+++ ripe/atlas/tools/renderers/ntp.py
@@ -13,15 +13,16 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from .base import Renderer as BaseRenderer
-from ..helpers.colours import colourise
 from tzlocal import get_localzone
 from datetime import datetime
+from .base import Renderer as BaseRenderer
+from ..helpers.colours import colourise
+
 
 class Renderer(BaseRenderer):
     RENDERS = [BaseRenderer.TYPE_NTP]
 
-    def on_result(self, result, probes=None):
+    def on_result(self, result):
         created = result.created.astimezone(get_localzone())
         probe_id = result.probe_id
         r = '\n\nProbe #{0}\n{1}\n'.format(probe_id, '=' * 79)
@@ -36,29 +37,38 @@ class Renderer(BaseRenderer):
         stratum = result.stratum
         v = result.version
         mode = result.mode
-        end_time = result.end_time
+        # just here for completeness, flake8 doesn't like when it's not used
+        # end_time = result.end_time
         poll = result.poll
         precision = result.precision
         refid = result.reference_id
         ref_time = result.reference_time
         root_delay = result.root_delay
         root_disp = result.root_dispersion
+
         if not leap and not stratum and not v and not mode:
             return
+
         if mode != 'server':
             print('invalid mode: %s' % mode)
-        r = '[NTP] %s -> %s (%s)\n' % (result.source_address, result.destination_name, result.destination_address)
-        r += '\tversion: %s, stratum: %s/16\n' % (colourise(v, 'bold'), colourise(stratum, 'bold'))
+
+        r = '[NTP] %s -> %s (%s)\n' % (result.source_address,
+                                       result.destination_name,
+                                       result.destination_address)
+        r += '\tversion: %s, stratum: %s/16\n' % (colourise(v, 'bold'),
+                                                  colourise(stratum, 'bold'))
         r += '\trefid: %s\n' % colourise(refid, 'bold')
         r += '\tleap: %s, poll: %s, precision: %s\n' % (leap, poll, precision)
         r += '\troot_delay: %s, root_disp: %s\n' % (root_delay, root_disp)
-        r += '\tref-time: %s\n' % datetime.fromtimestamp(ref_time)
+        r += '\tref-time: %s\n\n' % datetime.fromtimestamp(ref_time)
+
         try:
             for idx, pkt in enumerate(result.packets):
                 r += '\t[%s] %s\n' % (idx, str(pkt))
-                r += '\t\ttrans: %s -> recv: %s\n' % (pkt.transmitted_time, pkt.received_time)
-                r += '\t\torigin: %s -> final: %s\n' % (pkt.origin_time, pkt.final_time)
-
+                r += '\t\ttrans: %s -> recv: %s\n' % (pkt.transmitted_time,
+                                                      pkt.received_time)
+                r += '\t\torigin: %s -> final: %s\n\n' % (pkt.origin_time,
+                                                          pkt.final_time)
         except Exception as ex:
             print('Got exception when reading packet: %s' % ex)
             print('Raw: %s' % pkt.raw_data)
