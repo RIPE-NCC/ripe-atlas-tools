a39008c9c0cebb2adf1b4d7617aef1cc099398d2
Keith Gaughan <k@stereochro.me>
2018-06-24 12:32:31 +0100

    Proper XDG Base Directory Specification support.

---
+++ ripe/atlas/tools/helpers/xdg.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2016 RIPE NCC
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import os.path
+
+def get_config_home():
+    """
+    """
+    config_home = os.environ.get("XDG_CONFIG_HOME")
+    if config_home is None:
+        config_home = os.path.expanduser("~/.config")
+    return os.path.join(config_home, "ripe-atlas-tools")
