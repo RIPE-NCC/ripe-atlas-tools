1e10b6919a750cef36ce4f745181a5673b8d0eb8
Francesco Iannuzzelli <fiannuzzelli@ripe.net>
2025-10-28 17:48:58 +0100

    Remove Python 3.9 (EOL)

--- setup.py
+++ setup.py
@@ -60,7 +60,6 @@ setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: POSIX",
         "Operating System :: Unix",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
