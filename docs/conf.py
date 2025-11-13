aac129d372cf6ee274de778fb447557cd542afd1
Francesco Iannuzzelli <fiannuzzelli@ripe.net>
2025-11-13 16:56:02 +0100

    New Sphinx theme for docs

--- docs/conf.py
+++ docs/conf.py
@@ -43,6 +43,7 @@ extensions = [
     "sphinx.ext.todo",
     "sphinx.ext.imgmath",
     "sphinx.ext.viewcode",
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -119,7 +120,7 @@ pygments_style = "sphinx"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "default"
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
